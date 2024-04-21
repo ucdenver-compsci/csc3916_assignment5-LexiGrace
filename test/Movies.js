35e2b92ad20474c69319afd9ef54aac83f8e2902
Alexandra G. A. <143737426+LexiGrace@users.noreply.github.com>
2024-04-21 14:03:48 -0600

    Copied necessary Hw4 files into Hw5

---
+++ test/Movies.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB);
+
+const MovieSchema = new mongoose.Schema({
+  title: { type: String, required: true, index: true },
+    releaseDate: { type: String, required: true }, 
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+    },
+    actors: [{
+      actorName: String, 
+      characterName: String,
+    }],
+    imageUrl: { type: String }, 
+  }, { collection : 'movies' });
+  
+
+//return the model
+const MovieModel = mongoose.model('Movie', MovieSchema);
+module.exports = MovieModel;
