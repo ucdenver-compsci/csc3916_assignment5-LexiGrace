8f6065570278cc23997cdd45bd3b08d51d30d589
Alexandra G. A. <143737426+LexiGrace@users.noreply.github.com>
2024-05-02 16:17:11 -0600

    auth.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function getJSONObjectForMovieRequirement(req) {
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: '/Please include both username and password to signup.'})
+        res.json({success: false, msg: '/Please include both username and password.'})
     } else {
         var user = new User();
         user.name = req.body.name;
@@ -138,7 +138,7 @@ router.route('/movies')
                 console.log(err);
             // movie is found
             if (found.length != 0)
-                return res.json({success: false, msg: 'Movie already exists.'});
+                return res.json({success: false, msg: 'That movie already exists.'});
             else // movie is not found
             {
                 // Save a single movie
@@ -149,7 +149,7 @@ router.route('/movies')
                 newMovie.actors = req.body.actors;
 
                 if (newMovie.releaseDate < 1888 || newMovie.actors.length < 3 || !Movie.schema.path('genre').enumValues.includes(newMovie.genre))
-                    res.status(400).send({success: false, message: 'Unable to add film.'});
+                    res.status(400).send({success: false, message: 'Unable to add this film.'});
                 else
                 {
                     newMovie.save(function(err){
